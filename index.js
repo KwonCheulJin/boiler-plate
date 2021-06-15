1eeeb701dba07f9b1417b74aa896c043d840e4fe
KwonCheulJin <KwonCheulJin@users.noreply.github.com>
2021-06-15 17:28:46 +0900

    로그인 backend 완료

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 5000;
 const bodyParser = require('body-parser');
-const config = require('./config/key')
+const cookieParser = require('cookie-parser');
+const config = require('./config/key');
 
-const { User } = require("./models/User");
+const { auth } = require('./middleware/auth');
+const { User } = require('./models/User');
 
 
 // aplication/x-www-form-urlencoded
@@ -13,6 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // aplication/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false,
@@ -22,29 +25,80 @@ mongoose.connect(config.mongoURI, {
 
 
 
-
-
-
 app.get('/', (req, res) => res.send('Hello World!'));
 
 
 
+app.post('/api/users/register', (req, res) => {
 
-
-app.post('/register', (req, res) => {
   // 회원가입 할 때 필요한 정보들을 client에서 가져오면
   // 그것을 데이터 베이스에 저장한다.
 
   const user = new User(req.body);
 
   user.save((err, userInfo) => {
-    if (err) return res.json({ success: false, err })
+    if (err) return res.json({ success: false, err });
     return res.status(200).json({
       success: true,
-    })
+    });
+  });
+
+});
+
+app.post('/api/users/login', (req, res) => {
+
+  // 요청된 이메일을 DB에서 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+    // 요청된 이메일이 DB에 있다면 비밀번호가 맞는 비밀번호인지 확인한다.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      // console.log(err, isMatch)
+      if (!isMatch)
+        return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." });
+      // 비밀번호까지 맞다면 토큰을 생성한다.
+      user.generateToken((err, user) => {
+        // console.log(err, user)
+        if (err) return res.status(400).send(err);
+        // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지
+        res.cookie('x_auth', user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+
+      });
+    });
+  });
+});
+
+// role === 0이면 일반유저, role !== 0이면 관리자
+app.get('/api/users/auth', auth, (req, res) => {
+  console.log(req, res)
+  // 여기까지 미들웨어를 통과해서 왔다는 얘기는 Authentication이 true;
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    role: req.user.role,
+    image: req.user.image,
   })
-
-
 })
+
+app.get('/api/users/logout', auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id },
+    { token: "" },
+    (err, user) => {
+      if (err) return res.json({ success: false, err });
+      return res.status(200).send({
+        success: true,
+      });
+    });
+});
+
 
 app.listen(port, () => console.log(`Example app listenig on port ${port}!`));
